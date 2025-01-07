cb93446a40362b430edc3968d64ce758a2960f82
FaezehAlimoradi <faezehalimoradi8231@gmail.com>
2025-01-07 21:29:37 +0330

    fix

--- pages/_/customer/orders/new.vue
+++ pages/_/customer/orders/new.vue
@@ -1,5 +1,5 @@
 <template xmlns="http://www.w3.org/1999/html">
-  <div class="w-274 bg-blue new-order flex-column rounded-2xl pl-6">
+  <div class="w-274 pb-8 bg-[#FFFFFF] new-order flex-column rounded-2xl pl-6">
     <div class="pr-6 pt-8 text-3xl font-bold text-[#141A31]">ایجاد سفارش جدید</div>
     <div>
       <div class="flex-column pr-6 mt-8">
@@ -10,7 +10,8 @@
           >
             <div class="font-sm font-medium mb-2">مبدا</div>
             <v-textarea
-              label="One row"
+              prepend-inner-icon="mdi-map-marker-outline"
+              placeholder="لطفا آدرس مبدا خود راوارد کنید"
               row-height="10"
               rows="1"
               variant="outlined"
@@ -23,12 +24,12 @@
           >
             <div class="font-sm font-medium mb-2">مقصد</div>
             <v-textarea
-              placeholder="لطفاآدرس مقصد خود راوارد کنید"
+              prepend-inner-icon="mdi-map-marker"
+              placeholder="لطفا آدرس مقصد خود راوارد کنید"
               row-height="10"
               rows="1"
               variant="outlined"
               auto-grow
-              append-inner-icon="mdi-map-marker-up"
             ></v-textarea>
           </v-col>
           <v-col
@@ -37,7 +38,8 @@
           >
             <div class="font-sm font-medium mb-2">وزن</div>
             <v-textarea
-              label="Three rows"
+              prepend-inner-icon="mdi-scale"
+              placeholder="وزن بسته خود را وارد کنید"
               row-height="10"
               rows="1"
               variant="outlined"
@@ -50,24 +52,20 @@
             sm="6"
           >
             <div class="font-sm font-medium mb-2">نوع بسته ارسالی</div>
-            <v-textarea
-              label="لطفا آدرس محلی که بسته را تحویل میدهید بنویسید "
+            <v-select
+              clearable
+              placeholder="پاکت"
               row-height="8"
-              rows="1"
+              :items="['پاکت', 'جعبه کوچک','جعبه بزرگ','جعبه متوسط']"
               variant="outlined"
-              auto-grow
-              shaped
-            ></v-textarea>
+            ></v-select>
           </v-col>
           <v-col
-            cols="10"
-            sm="6"
+
           >
             <div class="font-sm font-medium mb-2">توضیحات</div>
             <v-textarea
-              label="لطفا آدرس محلی که بسته را تحویل میدهید بنویسید "
-              row-height="8"
-              rows="1"
+              placeholder="توضیحات بسته خود را اضافه کنید"
               variant="outlined"
               auto-grow
               shaped
@@ -77,7 +75,15 @@
 
       </div>
     </div>
-    bkadcbhja
+    <div class="flex justify-end">
+      <v-btn
+        class="font-sm font-medium"
+        size="large"
+
+
+        color="blue"
+      >ثبت سفارش</v-btn>
+    </div>
   </div>
 </template>
 
