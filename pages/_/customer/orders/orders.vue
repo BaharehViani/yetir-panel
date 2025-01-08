458eda4f526feaf4ea1ac6ea7c745e85d7899287
FaezehAlimoradi <faezehalimoradi8231@gmail.com>
2025-01-08 19:57:55 +0330

    minor fix

---
+++ pages/_/customer/orders/orders.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-274 pb-8 bg-[#FFFFFF] new-order flex-column rounded-2xl pl-6">
+
+    <v-data-table :items="items"></v-data-table>
+
+  </div>
+
+</template>
+
+<script setup>
+
+definePageMeta({
+  layout: 'panel'
+})
+
+const items = [
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests',
+  },
+  //
+]
+</script>
+
+
+
+<style scoped>
+
+</style>
