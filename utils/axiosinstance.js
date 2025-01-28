03e8ba7e631f3834e6c9dabdc6a895d81e6b09c8
Fatemeh <ftmalimoradi2@gmail.com>
2025-01-28 23:22:37 +0330

    api url changed

--- utils/axiosinstance.js
+++ utils/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create()
 
-axiosInstance.defaults.baseURL = 'http://yetri.api:8000/v1'
+axiosInstance.defaults.baseURL = 'https://yetirapi.skyrun.cloud/v1'
 axiosInstance.defaults.headers.common['Accept'] = 'application/json'
 
 export default axiosInstance
