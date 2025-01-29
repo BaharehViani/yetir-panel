e4a55cab673eabaed747d3a50a0ae4da25849981
Fatemeh <ftmalimoradi2@gmail.com>
2025-01-30 01:13:15 +0330

    check deploy

--- pages/login.vue
+++ pages/login.vue
@@ -166,7 +166,7 @@
       <v-card-subtitle
           class="text-center text-gray-600 text-lg font-semibold pb-8"
       >
-        برای ورود شماره تلفن همراه خود را به همراه گذرواژه وارد کنید.
+        برای ورود شماره تلفن همراه خود را به همراه رمز عبور وارد کنید.
       </v-card-subtitle>
       <v-form class="mt-4" fast-fail @submit.prevent="loginFormLogInHandler">
         <v-text-field
