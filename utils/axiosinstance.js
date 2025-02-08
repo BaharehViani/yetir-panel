f362575ebebf064cca110e199ac6c59bd0c1ab5c
bahareh <baharehviani@gmail.com>
2025-02-08 19:25:19 +0330

    baseURL fixed

--- utils/axiosinstance.js
+++ utils/axiosinstance.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const axiosInstance = axios.create()
 
-//axiosInstance.defaults.baseURL = 'https://yetirapi.skyrun.cloud/v1'
-axiosInstance.defaults.baseURL = 'http://127.0.0.1:8000/v1'
+axiosInstance.defaults.baseURL = 'https://yetirapi.skyrun.cloud/v1'
+//axiosInstance.defaults.baseURL = 'http://127.0.0.1:8000/v1'
 
 axiosInstance.defaults.headers.common['Accept'] = 'application/json'
 
