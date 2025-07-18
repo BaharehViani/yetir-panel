9227cc91992afe849785aab0d25dc0eadc158a97
bahareh <baharehviani@gmail.com>
2025-07-19 02:24:01 +0330

    domain for backend URL

--- utils/axiosinstance.js
+++ utils/axiosinstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create()
 
 //axiosInstance.defaults.baseURL = 'https://yetirapi.skyrun.cloud/v1'
 // axiosInstance.defaults.baseURL = 'http://127.0.0.1:8000/v1'
-axiosInstance.defaults.baseURL = 'https://yetir.kojiberi.com/v1'
+axiosInstance.defaults.baseURL = 'https://yetirapi.kojiberi.com/v1'
 
 axiosInstance.defaults.headers.common['Accept'] = 'application/json'
 
